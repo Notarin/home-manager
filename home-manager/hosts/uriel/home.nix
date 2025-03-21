35491ce629bdb93b82bba23ef6108a09c4506d62
Notarin Steele <424c414e4b@gmail.com>
2025-03-21 00:57:01 -0400

    Fixed monitor refresh rates and added TV

--- home-manager/hosts/uriel/home.nix
+++ home-manager/hosts/uriel/home.nix
@@ -14,7 +14,8 @@
   home.stateVersion = "24.05";
 
   wayland.windowManager.hyprland.settings.monitor = [
-    "DP-1,1920x1080@144,0x0,1"
-    "DP-2,1920x1080@144,1920x-420,1,transform,1"
+    "DP-1,1920x1080@60,0x0,1"
+    "DP-2,1920x1080@60,1920x-420,1,transform,1"
+    "HDMI-A-1,1920x1080@60,0x0,1,mirror,DP-1"
   ];
 }
