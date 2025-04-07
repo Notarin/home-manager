dfd76de90d6487ae5112ac9fc0a8636a5a6e3444
Notarin Steele <424c414e4b@gmail.com>
2025-04-07 05:04:25 -0400

    Removed unused debug user

--- home-manager/users/debug.nix
+++
@@ -1,8 +0,0 @@
-{ ... }:
-
-{
-  # The following is a best-guess at likely defaults for a new generic system.
-  home.username = "debug";
-  home.homeDirectory = "/home/debug";
-  home.stateVersion = "24.05";
-}
