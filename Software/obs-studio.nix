53a6fe540072bd83fa0e6d6414a2ff3f0a3bf12d
Notarin Steele <424c414e4b@gmail.com>
2025-05-19 14:32:59 -0400

    Added OBS-Studio plugins

--- Software/obs-studio.nix
+++ Software/obs-studio.nix
@@ -1,3 +1,10 @@
-{...}: {
+{pkgs, ...}: {
   enable = true;
+  plugins = with pkgs.obs-studio-plugins; [
+    wlrobs
+    obs-vkcapture
+    obs-vaapi
+    obs-tuna
+    obs-pipewire-audio-capture
+  ];
 }
