ad58debef6b3581c35161c9c5251baeb606ce5b6
Notarin Steele <424c414e4b@gmail.com>
2026-04-14 19:34:42 -0400

    Dropped the bloaty devshell.

--- flake.nix
+++ flake.nix
@@ -70,21 +70,6 @@
     in {
       formatter.${system} = pkgs.callPackage ./format.nix {};
       checks.${system}.formatting = self.formatter.${system};
-      devShells.${system}.default = pkgs.mkShell {
-        shellHook = ''
-          oldHookDir=$(git config --local core.hooksPath)
-
-          if [ "$oldHookDir" != "$PWD/.githooks" ]; then
-            read -rp "Set git hooks to $PWD/.githooks? (y/n) " answer
-            if [ "$answer" = "y" ]; then
-              git config core.hooksPath "$PWD"/.githooks
-              echo "Set git hooks to $PWD/.githooks"
-            else
-              echo "Skipping git hooks setup"
-            fi
-          fi
-        '';
-      };
       packages.${system} = {
         hydrus-client = pkgs.symlinkJoin {
           name = "hydrus-client";
