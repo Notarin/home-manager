f3848d5fef518c85b1483004e3e83188a28350fe
Notarin Steele <424c414e4b@gmail.com>
2026-04-22 03:05:36 -0400

    Updated snix to use a FOD instead of eval-time fetcher

--- packages/default.nix
+++ packages/default.nix
@@ -2,11 +2,13 @@ pkgs: let
   callPackage = (pkgs.extend (_: _: packages)).callPackage;
   packages = {
     hydrus-client = callPackage ./hydrus-client.nix {};
+    # TODO: Discovered an issue where snixs binary isn't in the expected location, thus cannot run from Nix CLI.
     snix-cli =
-      (pkgs.callPackage "${(builtins.fetchGit {
-        url = "git+https://git.snix.dev/snix/snix";
+      (pkgs.callPackage "${(pkgs.fetchgit {
+        url = "https://git.snix.dev/snix/snix";
         rev = "b0a9664987faa8c9331a54cd91ea7889c71bfe64";
-      })}/default.nix" {localSystem = pkgs.stdenv.system;}).snix.cli.eval;
+        sha256 = "sha256-YEjXDFFITuLVnGWNFD7KfBpmCvlBh6d2jp6YiPvM1Rs=";
+      })}" {localSystem = pkgs.stdenv.system;}).snix.cli.eval;
   };
 in {
   inherit (packages) hydrus-client snix-cli;
