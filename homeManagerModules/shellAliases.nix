028eb905c819213347a90773087f386068cc4dac
Notarin Steele <424c414e4b@gmail.com>
2026-05-01 08:15:50 -0400

    Removed some unused shell aliases

--- homeManagerModules/shellAliases.nix
+++ homeManagerModules/shellAliases.nix
@@ -10,11 +10,9 @@
     description = "Shell aliases to be added to all shells.";
   };
   config.shellAliases = rec {
-    q = "exit"; # `q` for "quit"
     l = "ls";
     la = "${l} -la";
     c = "clear";
-    helix = "hx"; # I keep accidentally typing `helix` instead of `hx`
     edit = config.home.sessionVariables.VISUAL;
     cat = lib.getExe pkgs.bat;
   };
