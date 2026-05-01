d0c532296c15d89a67aaeb89de9b3e425e741547
Notarin Steele <424c414e4b@gmail.com>
2026-05-01 05:19:49 -0400

    Tiny grammar fix in flake description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Home Manager configuration of Notarin";
+  description = "Home Manager configuration for Notarin";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
