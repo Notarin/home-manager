c8df0a01bea4dc94fe793eecbb06f4f599537fc3
Notarin Steele <424c414e4b@gmail.com>
2026-09-19 02:31:47 -0400

    Second stage migration Hyprlang -> Lua

--- homeManagerModules/programs/hyprland.nix
+++ homeManagerModules/programs/hyprland.nix
@@ -35,6 +35,10 @@
     systemd.variables = ["--all"];
     settings =
       {
+        #input = {
+        #  kb_options = "caps:super";
+        #};
+
         workspace_rule = [
           (lib.generators.mkLuaInline "{ workspace = \"w[tv1]\", gaps_out = 0, gaps_in = 0, no_border = true }")
           (lib.generators.mkLuaInline "{ workspace = \"f[1]\", gaps_out = 0, gaps_in = 0, no_border = true }")
@@ -47,19 +51,63 @@
               (lib.generators.mkLuaInline command)
             ];
           };
+          bindl = bindName: command: {
+            _args = [
+              bindName
+              (lib.generators.mkLuaInline command)
+              (lib.generators.mkLuaInline "{ locked = true }")
+            ];
+          };
+          bindel = bindName: command: {
+            _args = [
+              bindName
+              (lib.generators.mkLuaInline command)
+              (lib.generators.mkLuaInline "{ locked = true, [\"repeat\"] = true }")
+            ];
+          };
         in
           [
+            # Audio / Media Binds
+            (bindl "XF86AudioPlay" "hl.dsp.exec_raw(\"${lib.getExe pkgs.playerctl} play-pause\")")
+            (bindl "XF86AudioPrev" "hl.dsp.exec_raw(\"${lib.getExe pkgs.playerctl} previous\")")
+            (bindl "XF86AudioNext" "hl.dsp.exec_raw(\"${lib.getExe pkgs.playerctl} next\")")
+            (bindl "XF86AudioMute" "hl.dsp.exec_raw(\"wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle\")")
+            (bindel "XF86AudioRaiseVolume" "hl.dsp.exec_raw(\"wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%+\")")
+            (bindel "XF86AudioLowerVolume" "hl.dsp.exec_raw(\"wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%-\")")
+
+            # General Binds
             (bind "SUPER + code:36" "hl.dsp.exec_raw(\"${lib.getExe pkgs.wezterm}\")")
             (bind "SUPER + E" "hl.dsp.exec_raw(\"${lib.getExe pkgs.nautilus}\")")
             (bind "SUPER + D" "hl.dsp.exec_raw(\"${lib.getExe pkgs.fuzzel}\")")
+            #(bind "SUPER + SPACE" "hl.dsp.window.toggle_floating()")
+            #(bind "SUPER + J" "hl.dsp.window.toggle_split()")
+            (bind "SUPER + F" "hl.dsp.window.fullscreen(0)")
+            (bind "F11" "hl.dsp.window.fullscreen(0)")
+            (bind "SUPER + L" "hl.dsp.exec_raw(\"${lib.getExe pkgs.hyprlock}\")")
             (bind "SUPER + Q" "hl.dsp.window.close()")
             (bind "SUPER + K" "hl.dsp.exit()")
             (bind "SUPER + G" "hl.dsp.group.toggle()")
-            (bind "SUPER + SHIFT + S" "hl.dsp.exec_cmd(\"${lib.getExe pkgs.grim} -g \" .. '\"' .. \"$(${lib.getExe pkgs.slurp})\" .. '\"' .. \" - | ${lib.getExe' pkgs.wl-clipboard "wl-copy"}\")")
 
+            # Screenshot & Notification
+            (bind "SUPER + SHIFT + S" "hl.dsp.exec_cmd(\"${lib.getExe pkgs.grim} -g \" .. '\"' .. \"$(${lib.getExe pkgs.slurp})\" .. '\"' .. \" - | ${lib.getExe' pkgs.wl-clipboard "wl-copy"}\")")
+            (bind "Print" "hl.dsp.exec_cmd(\"${lib.getExe pkgs.grim} -t jpeg - | ${lib.getExe pkgs.imv} -f -\")")
+            (bind "SUPER + N" "hl.dsp.exec_raw(\"${lib.getExe' pkgs.swaynotificationcenter "swaync-client"} -t\")")
+
+            # Moving Focus
+            (bind "SUPER + LEFT" "hl.dsp.focus({ direction = \"l\" })")
+            (bind "SUPER + RIGHT" "hl.dsp.focus({ direction = \"r\" })")
+            (bind "SUPER + UP" "hl.dsp.focus({ direction = \"u\" })")
+            (bind "SUPER + DOWN" "hl.dsp.focus({ direction = \"d\" })")
+
+            # Moving Windows (Or to group)
+            #(bind "SUPER + SHIFT + LEFT" "hl.dsp.window.move_or_group({ direction = \"l\" })")
+            #(bind "SUPER + SHIFT + RIGHT" "hl.dsp.window.move_or_group({ direction = \"r\" })")
+            #(bind "SUPER + SHIFT + UP" "hl.dsp.window.move_or_group({ direction = \"u\" })")
+            #(bind "SUPER + SHIFT + DOWN" "hl.dsp.window.move_or_group({ direction = \"d\" })")
+
+            # Workspace / Group scrolling
             (bind "SUPER + mouse_down" "hl.dsp.focus({ workspace = \"r-1\"})")
             (bind "SUPER + mouse_up" "hl.dsp.focus({ workspace = \"r+1\"})")
-
             (bind "SUPER + Tab" "hl.dsp.group.next()")
             (bind "SUPER + SHIFT + Tab" "hl.dsp.group.prev()")
 
@@ -81,6 +129,8 @@
           ++ lib.optionals (config.host == "uriel") [
             (bind "SUPER + CONTROL + LEFT" "hl.dsp.focus({ workspace = \"e-1\"})")
             (bind "SUPER + CONTROL + RIGHT" "hl.dsp.focus({ workspace = \"e+1\"})")
+            #(bind "SUPER + ALT + RIGHT" "hl.dsp.workspace.move_to_monitor(\"HDMI-A-1\")")
+            #(bind "SUPER + ALT + LEFT" "hl.dsp.workspace.move_to_monitor(\"DP-1\")")
           ];
       }
       // lib.optionalAttrs (config.host == "uriel") {
